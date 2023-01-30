b55032a95fc74dae6c5bbd682d2794f65cb520d5
Spola1 <1111.22224@mail.ru>
2023-01-30 11:34:29 +0300

    add room model && controller actions create/new

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,4 +1,25 @@
 class RoomsController < ApplicationController
   def index
   end
+
+  def new
+    @room = Room.new
+  end
+
+  def create
+    @room = current_user.rooms.build(room_params)
+
+    if @room.save
+      flash[:success] = 'Room created!'
+      redirect_to(root_path)
+    else
+      render(:new)
+    end
+  end
+
+  private
+
+  def room_params
+    params.require(:room).permit(:title)
+  end
 end
